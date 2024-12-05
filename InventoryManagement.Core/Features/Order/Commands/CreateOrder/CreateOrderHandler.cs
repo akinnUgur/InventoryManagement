cb368911511593705f32858e471f9c4a04df86fc
Uğur Akın <126957768+akinnUgur@users.noreply.github.com>
2024-12-06 00:43:22 +0300

    Product Stock Update after order implemented

--- InventoryManagement.Core/Features/Order/Commands/CreateOrder/CreateOrderHandler.cs
+++ InventoryManagement.Core/Features/Order/Commands/CreateOrder/CreateOrderHandler.cs
@@ -15,32 +15,59 @@ namespace InventoryManagement.Core.Features.Order.Commands.CreateOrder
     public class CreateOrderHandler : IRequestHandler<CreateOrderRequest, ApiResponse<CreateOrderResponse>>
     {
         private readonly IOrderRepository _orderRepository;
+        private readonly IProductRepository _productRepository;
 
-        public CreateOrderHandler(IOrderRepository orderRepository)
+        public CreateOrderHandler(IOrderRepository orderRepository, IProductRepository productRepository)
         {
             _orderRepository = orderRepository;
-
+            _productRepository = productRepository;
         }
 
         async Task<ApiResponse<CreateOrderResponse>> IRequestHandler<CreateOrderRequest, ApiResponse<CreateOrderResponse>>.Handle(CreateOrderRequest request, CancellationToken cancellationToken)
         {
+            // Sipariş oluşturuluyor
             var order = new Entities.Order
             {
                 Email = request.Email,
                 OrderDate = DateTime.UtcNow,
                 Status = OrderStatus.Pending,
-                OrderItems = request.Items.Select(i => new OrderItem
-                {
-                    ProductId = i.ProductId,
-                    Quantity = i.Quantity,
-                    TotalPrice = i.TotalPrice * i.Quantity,
-                }).ToList()
+                OrderItems = new List<OrderItem>()
             };
 
+            foreach (var item in request.Items)
+            {
+           
+                var product = await _productRepository.GetByIdAsync(item.ProductId);
 
+                if (product == null)
+                {
+                    return ApiResponse<CreateOrderResponse>.Error("Product not found", 404);
+                }
 
+              
+                if (product.Stock < item.Quantity)
+                {
+                    return ApiResponse<CreateOrderResponse>.Error($"Not enough stock for product {product.Name}", 400);
+                }
+
+           
+                var orderItem = new OrderItem
+                {
+                    ProductId = item.ProductId,
+                    Quantity = item.Quantity,
+                    TotalPrice = product.Price * item.Quantity
+                };
+
+                order.OrderItems.Add(orderItem);
+
+               
+                product.Stock -= item.Quantity;
+                await _productRepository.Update(product);  
+
+            }
+
+            // Siparişi veritabanına kaydediyoruz
             await _orderRepository.AddAsync(order);
-
 
             var response = new CreateOrderResponse
             {
@@ -48,7 +75,6 @@ namespace InventoryManagement.Core.Features.Order.Commands.CreateOrder
             };
 
             return ApiResponse<CreateOrderResponse>.Success(response, "Order created successfully", 201);
-
         }
     }
 }
--- InventoryManagement.Core/Features/Order/Commands/CreateOrder/CreateOrderRequest.cs
+++ InventoryManagement.Core/Features/Order/Commands/CreateOrder/CreateOrderRequest.cs
@@ -11,8 +11,14 @@ namespace InventoryManagement.Core.Features.Order.Commands.CreateOrder
     public class CreateOrderRequest: IRequest<ApiResponse<CreateOrderResponse>>
     {
         public string Email { get; set; }
-        public List<OrderItemDTO> Items { get; set; }
+        public List<OrderItemRequest> Items { get; set; }
 
 
+    }
+
+    public class OrderItemRequest
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
     }
 }

cd077a3ccee83f1295acdbdf22991bd32a624e44
Uğur Akın <126957768+akinnUgur@users.noreply.github.com>
2024-12-05 15:13:09 +0300

    Add project files.

---
+++ InventoryManagement.Core/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Core.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ParentCategoryId { get; set; }
+        public Category? ParentCategory { get; set; }
+        public ICollection<Category> SubCategories { get; set; } = new List<Category>();
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ InventoryManagement.Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using InventoryManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Infrastructure.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Inventory> Inventory { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("InMemoryDatabase");
+        }
+    }
+}
+
